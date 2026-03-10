d14d256163c393c665877212555d8e1c62de0238
João Vitor <joao7791vitor@gmail.com>
2026-03-09 23:44:55 -0300

    refactor: remove unused P2002 error handler

--- src/middlewares/errorHandlerMiddleware.ts
+++ src/middlewares/errorHandlerMiddleware.ts
@@ -30,20 +30,6 @@ const errorHandlerMiddleware: ErrorRequestHandler = (
     return res.status(400).json({ message: `Erro no upload: ${error.message}` })
   }
 
-  if (error.code === "P2002") {
-    const translations: Record<string, string> = {
-      sku: "SKU",
-      email: "E-mail",
-      phone: "Telefone"
-    }
-
-    const fields = error.meta?.target as string[] || []
-    const filteredFields = fields.filter(field => field !== 'stockId')
-    const target = filteredFields.map(f => translations[f] || f).join(", ")
-
-    return res.status(409).json({ message: `Já existe um registro com este ${target}.`, })
-  }
-
   return res.status(500).json({ message: "Internal server error." })
 }
 
