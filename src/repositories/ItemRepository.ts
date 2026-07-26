e464459c536f57e30134c726147dc8a9d11e7c85
João Vitor <joao7791vitor@gmail.com>
2026-07-25 21:46:10 -0300

    fix: include category color and icon in item response

--- src/repositories/ItemRepository.ts
+++ src/repositories/ItemRepository.ts
@@ -68,7 +68,9 @@ export const itemRepository = {
         category: {
           select: {
             id: true,
-            name: true
+            name: true,
+            color: true,
+            iconName: true
           }
         },
 
